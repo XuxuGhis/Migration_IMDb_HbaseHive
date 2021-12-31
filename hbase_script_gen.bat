ccd4a47e428c74d1c08dc8dbffd94e9dbb7a331a
sunnyyanggithub <71558890+sunnyyanggithub@users.noreply.github.com>
2021-12-31 18:22:09 +0100

    Update and rename script_test.bat to hbase_script_gen.bat

---
+++ hbase_script_gen.bat
@@ -0,0 +1,16 @@
+@echo off
+
+set file=C:\Users\Sunny\Desktop\title_basics.tsv
+
+set members=for /f "tokens=1-9 delims=;" %%A in (%file) do (set members=!members! + "%%A")
+
+for /f "tokens=1-9 delims=;" %%A in (%file%) do (
+echo put table_imdb "%%F+%%I+%%D", "%%A:titleType", "%%B"
+echo put table_imdb "%%F+%%I+%%D", "%%A:primaryTitle", "%%C"
+echo put table_imdb "%%F+%%I+%%D", "%%A:originalTitle", "%%D"
+echo put table_imdb "%%F+%%I+%%D", "%%A:colisAdult", %%E"
+echo put table_imdb "%%F+%%I+%%D", "%%A:startYear", %%F"
+echo put table_imdb "%%F+%%I+%%D", "%%A:endYear", %%G"
+echo put table_imdb "%%F+%%I+%%D", "%%A:runtimeMinutes", %%H"
+echo put table_imdb "%%F+%%I+%%D", "%%A:genres", %%I"
+)
